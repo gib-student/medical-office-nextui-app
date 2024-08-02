217643452292181c85d2d9e816ce90579f3d150c
Trevor Gibb <gib19021@byui.edu>
2024-08-01 21:51:03 -0600

    update

--- components/Home/GetCareNow.tsx
+++ components/Home/GetCareNow.tsx
@@ -2,8 +2,8 @@ import { Image } from "@nextui-org/image";
 
 export default function GetCareNow() {
   return (
-    <div className="">
-      <Image width={300} src="https://placehold.co/100"></Image>
+    <div className="w-full border-2 border-red-500 mb-5">
+      <Image className="w-full" src="https://placehold.co/1000"></Image>
     </div>
   );
 }
