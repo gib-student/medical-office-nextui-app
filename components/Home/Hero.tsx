cba7a2a6ac335837d144d277119cedc3bde8b012
Trevor Gibb <gib19021@byui.edu>
2024-08-02 19:01:06 -0600

    update

--- components/Home/Hero.tsx
+++ components/Home/Hero.tsx
@@ -8,7 +8,7 @@ export default function Hero() {
   return (
     <>
       <div className="relative">
-        <div className="h-8 w-full top-0 absolute z-10 bg-gradient-to-b from-white" />
+        <div className="h-1 w-full top-0 absolute z-10 bg-gradient-to-b from-white" />
         <Image
           alt="Hero photo"
           className="z-0"
