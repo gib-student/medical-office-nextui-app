88b3427ab1d3f1a1c1aba50d6c866d44ec347c7f
Trevor Gibb <gib19021@byui.edu>
2024-08-09 14:18:25 -0600

    finished myChartInfo

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 "use client";
+import { Button } from "@nextui-org/button";
 import FeaturedServices from "@/components/Home/FeaturedServices";
-import Hero from "@/components/Home/Hero";
-import SecondNav from "@/components/Home/SecondNav";
 import GetCareNow from "@/components/Home/GetCareNow";
-import ServicesAndSupportInfo from "@/components/Home/ServicesAndSupportInfo";
+import Hero from "@/components/Home/Hero";
 import MyChartInfo from "@/components/Home/MyChartInfo";
+import SecondNav from "@/components/Home/SecondNav";
+import ServicesAndSupportInfo from "@/components/Home/ServicesAndSupportInfo";
 
 export default function Home() {
   return (
@@ -15,6 +16,12 @@ export default function Home() {
       <GetCareNow />
       <ServicesAndSupportInfo />
       <MyChartInfo />
+      <div className="h-14"></div>
+      <div className="fixed bottom-0 right-0 left-0 bg-white p-2 z-50">
+        <Button className="rounded-md bg-blue-900 text-white w-full">
+          Get Care Now
+        </Button>
+      </div>
     </>
   );
 }
