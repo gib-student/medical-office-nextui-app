21a1203f3b0aa88a3409cfd97a85640cea01c5c1
Trevor Gibb <gib19021@byui.edu>
2025-03-12 22:33:16 -0600

    update

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,7 +1,11 @@
+"use client";
+
+import LoginForm from "@/components/LoginForm";
+
 export default function LoginPage() {
   return (
     <>
-      <div>Hello</div>
+      <LoginForm></LoginForm>
     </>
   );
 }
