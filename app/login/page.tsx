c6d0320fca64cfe75e756354f4a55b6c8b408a3e
Trevor Gibb <gib19021@byui.edu>
2025-03-31 15:12:22 -0600

    update
    update

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,59 +2,47 @@
 
 import LoginForm from "@/components/LoginForm";
 import { db } from "@/firebase";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { collection, query, where, getDocs } from "firebase/firestore";
 import { useRouter } from "next/navigation";
 
 export default function LoginPage() {
   const router = useRouter();
-
-  // Query for the user
-  const getPatientID = async (username: string, password: string) => {
-    const usersRef = collection(db, "users");
-    const q = query(usersRef, where("username", "==", username));
-    const querySnapshot = await getDocs(q);
-    if (querySnapshot.empty) {
-      console.log("No matching documents.");
-      return;
-    } else {
-      const userData = querySnapshot.docs[0].data();
-      if (userData.password === password) {
-        return userData.patient_id;
-      } else {
-        console.log("Incorrect password.");
-        return null;
-      }
-    }
-  };
-
-  const getPatientData = async (patientID: string) => {
-    const patientsRef = collection(db, "patients");
-    const q = query(patientsRef, where("patient_id", "==", patientID));
-    const querySnapshot = await getDocs(q);
-    if (querySnapshot.empty) {
-      console.log("No matching documents.");
-      return;
-    } else {
-      const patientData = querySnapshot.docs[0].data();
-      return patientData;
-    }
-  };
+  const auth = getAuth();
 
   // Call this function when they click the "Sign In" button
   const handleLogin = async (username: string, password: string) => {
-    const patientID = await getPatientID(username, password);
-    if (patientID) {
-      console.log("User authenticated:", patientID);
-      // Store the patient information in local storage for later use
-      const patientData = await getPatientData(patientID);
-      localStorage.setItem("patientID", patientID);
-      // Redirect to the dashboard or another page
-      router.push("/dashboard");
-    } else {
-      console.log("Authentication failed.");
-      // Handle authentication failure (e.g., show an error message)
-      alert("Invalid username or password. Please try again.");
-    }
+    // Google Authentication
+    signInWithEmailAndPassword(auth, username, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        // log the user information for debugging
+        console.log("User signed in:", user);
+        // Save the user information in local storage for later use
+        localStorage.setItem("user", JSON.stringify(user));
+        router.push("/dashboard");
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+      });
+
+    // Pre-Google Authentication
+    // const patientID = await getPatientID(username, password);
+    // if (patientID) {
+    //   console.log("User authenticated:", patientID);
+    //   // Store the patient information in local storage for later use
+    //   const patientData = await getPatientData(patientID);
+    //   localStorage.setItem("patientID", patientID);
+    //   // Redirect to the dashboard or another page
+    //   router.push("/dashboard");
+    // } else {
+    //   console.log("Authentication failed.");
+    //   // Handle authentication failure (e.g., show an error message)
+    //   alert("Invalid username or password. Please try again.");
+    // }
   };
 
   return (
